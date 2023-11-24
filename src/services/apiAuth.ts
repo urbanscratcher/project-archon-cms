fb90d1d591addc7ea5bdb5805b3079b6319bb964
Joun <urbanscratcher@gmail.com>
2023-11-24 22:09:24 +0900

    api added

--- src/services/apiAuth.ts
+++ src/services/apiAuth.ts
@@ -5,16 +5,8 @@ export type SignInReq = {
   password: string;
 };
 
-// const authConfig: InternalAxiosRequestConfig<any> = undefined;
-
 const authApi = {
   signin: (data: SignInReq) => request.post<SignInReq>('/auth', data),
 };
-
-// const users = {
-//   list: () => request.get<User[]>('/users'),
-//   details: (id: string) => request.get<User>(`/users/${id}`),
-//   create: (data: User) => request.post<User>('/users', data),
-// };
 
 export default authApi;
--- src/services/apiUser.ts
+++ src/services/apiUser.ts
@@ -2,6 +2,15 @@ import { request } from './axiosSetting';
 
 const userApi = {
   getList: () => request.get('/users'),
+  getDetail: (idx: number) => request.get(`/users/${idx}`),
+  delete: (idx: number) => request.delete(`/users/${idx}`),
+  update: (idx: number, body: any) => request.patch(`/users/${idx}`, body),
 };
+
+// const users = {
+//   list: () => request.get<User[]>('/users'),
+//   details: (id: string) => request.get<User>(`/users/${id}`),
+//   create: (data: User) => request.post<User>('/users', data),
+// };
 
 export default userApi;
